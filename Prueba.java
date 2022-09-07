40739e2960705204ee4c04d5be1da11b46d40d43
Erika <eperezg023@alumno.uaemex.mx>
2022-09-07 16:16:04 -0500

    Initial commit

---
+++ Prueba.java
@@ -0,0 +1,14 @@
+package figuras;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Figura[] figs=new Figura[3];
+        figs[0]=new Cuadrado(2);
+        figs[1]=new Triangulo(4,4,4);
+        figs[2]=new Cuadrado(4);
+
+        for(Figura fg:figs){
+            System.out.println(fg.toString());
+        }
+    }
+}
